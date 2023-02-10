077b9fe70aff19c45c7b92e65ab43122bfb79faf
StevenChafei <chafei.s@live.fr>
2023-02-10 17:00:02 +0100

    push

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import logo from "../assets/img/vinted_logo.png";
+import logo from "../assets/img/logovinted.png";
 import { Link } from "react-router-dom";
 
 const Header = ({ handleToken, userToken }) => {
